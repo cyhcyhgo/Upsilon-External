6941f70c1ff06c3d0433afc0025cc49e245efbe3
程英昊 <80203701+cyhcyhgo@users.noreply.github.com>
2025-12-14 17:17:27 +0800

    Update main.cpp

--- apps/KhiCAS/src/main.cpp
+++ apps/KhiCAS/src/main.cpp
@@ -193,12 +193,23 @@ int getkey(int allow_suspend) {
   return extapp_getKey(allow_suspend, &alphawasactive_);
 }
 
+bool iskeydown(int key) {
+  return extapp_isKeydown(key);
+}
+
 void GetKey(int * key) {
   *key = getkey(true);
 }
 
 bool inexammode(){
   return extapp_inexammode();
+}
+
+// 修复 newlib-nano 缺失 __ssputws_r 的问题
+int __ssputws_r(void * _reent, const wchar_t * wstr, void * file) {
+    // 这里不需要通过，直接返回 0 (EOF) 或者错误即可
+    // 因为在计算器上我们极大概率不需要宽字符打印到流
+    return 0; 
 }
 
 } // end extern "C"
